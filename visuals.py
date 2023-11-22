51a043467b67a3fb893d28b37f802b51a5febdff
Avika Pulges <a.pulges0620221@arts.ac.uk>
2023-11-22 18:22:28 +0000

    nearly there

--- visuals.py
+++ visuals.py
@@ -1,39 +1,107 @@
 import pyglet
 import math
+import time
+from pyglet.shapes import Circle
+import json
 
-# Close any open windows (if any exist)
-for window in pyglet.app.windows:
-    window.close()
+def visualise_body(all_frames, max_x, max_y, max_frames=500):
+    # Pyglet window initialization
+    window = pyglet.window.Window(int(max_x) + 50, int(max_y) + 50)
 
-# Create the window first
-window = pyglet.window.Window(800, 600)  # Set the window size to 800x600
+    # Define the limb connections based on keypoints
+    limb_connections = [
+        (0, 1), (1, 2), (2, 3), (3, 4),
+        (1, 5), (5, 6), (6, 7),
+        (1, 8), (8, 9), (9, 10), (10, 11),
+        (8, 12), (12, 13), (13, 14),
+        (0, 15), (15, 16), (0, 17), (17, 18),
+        (14, 19), (14, 20), (14, 21),
+        (11, 22), (11, 23), (11, 24)
+    ]
 
-# Set the window background color to black
-pyglet.gl.glClearColor(0,0,0,0)
+    # Load the same image for all limb connections
+    limb_image_path = 'data/leg.png'
+    limb_sprites = {limb: pyglet.sprite.Sprite(pyglet.image.load(limb_image_path)) for limb in limb_connections}
 
-# Load the image
-image = pyglet.image.load('data/leg.png')
-sprite = pyglet.sprite.Sprite(image)
+    # Function to draw each frame
+    def draw_frame(frame_data):
+        
+        for i in range(25):  # Assuming 25 keypoints
+            x = frame_data[i * 2]
+            y = frame_data[i * 2 + 1]
+            circle = Circle(x, y, 5, color=(0, 255, 0))
+            circle.draw()
+            
+        for limb, sprite in limb_sprites.items():
+            start_idx, end_idx = limb
+            start_x, start_y = frame_data[start_idx * 2], frame_data[start_idx * 2 + 1]
+            end_x, end_y = frame_data[end_idx * 2], frame_data[end_idx * 2 + 1]
+            
+            # Reset sprite properties
+            sprite.x = 0
+            sprite.y = 0
+            sprite.rotation = 0
+            sprite.scale = 1
 
-# Set the position and scale of the image
-coord1 = (200, 500)  # Replace with your first coordinate
-coord2 = (200, 100)  # Replace with your second coordinate
+            # Calculate midpoint, angle, and distance
+            mid_x, mid_y = (start_x + end_x) / 2, (start_y + end_y) / 2
+            angle = math.atan2(end_y - start_y, end_x - start_x)
+            distance = math.sqrt((end_x - start_x) ** 2 + (end_y - start_y) ** 2)
 
-sprite.x = min(coord1[0], coord2[0])
-sprite.y = min(coord1[1], coord2[1])
+            # Set sprite properties
+            sprite.x, sprite.y = mid_x - sprite.width / 2, mid_y - sprite.height / 2
+            sprite.rotation = -math.degrees(angle)
+            
+            if sprite.width ==0:
+                sprite.width = 1
+                print('width is 0')
+            
+            sprite.scale = distance / max(sprite.width, sprite.height)
+            
 
-# Calculate the angle and set the rotation of the sprite
-angle = math.atan2(coord2[1] - coord1[1], coord2[0] - coord1[0])
-sprite.rotation = -math.degrees(angle)
+            sprite.draw()
 
-# Calculate the distance between the two points and set the scale of the sprite
-distance = math.sqrt((coord2[0] - coord1[0])**2 + (coord2[1] - coord1[1])**2)
-sprite.scale = distance / max(sprite.width, sprite.height)
 
-@window.event
-def on_draw():
-    window.clear()
-    sprite.draw()
+    # Pyglet draw event
+    @window.event
+    def on_draw():
+        window.clear()
+        if frame_index < len(all_frames):
+            draw_frame(all_frames[frame_index])
+
+    # Update function for animation
+    def update(dt):
+        nonlocal frame_index
+        frame_index += 1
+        if frame_index >= len(all_frames):
+            pyglet.app.exit()
+
+    # Schedule update
+    pyglet.clock.schedule_interval(update, 0.5)
+
+    # Run the Pyglet application
+    frame_index = 0
+    pyglet.app.run()
+
 
 if __name__ == '__main__':
-    pyglet.app.run()    
+    
+    
+    # Read data from a JSON file
+    with open('data/data.json', 'r') as file:
+        loaded_data = json.load(file)
+
+    # Accessing the data
+    unnorm_out = loaded_data["unnorm_out"]
+    min_x = loaded_data["min_x"]
+    max_x = loaded_data["max_x"]
+    min_y = loaded_data["min_y"]
+    max_y = loaded_data["max_y"]
+    # Access other variables in a similar way
+
+
+    # Example usage
+    all_frames = unnorm_out # Your frames data
+
+
+    visualise_body(all_frames, max_x, max_y)
