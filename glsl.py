7917683906127809cb7d0e3f6a55eced34fc1064
Avika Pulges <a.pulges0620221@arts.ac.uk>
2023-11-30 01:20:56 +0000

    fixing fear

--- glsl.py
+++ glsl.py
@@ -314,133 +314,162 @@ def select_shader(emotion):
         #define MOD3 vec3(443.8975, 397.2973, 491.1871) // uv range
         #define PI 3.14159265
         
+        #define res resolution.xy
+
+
+        #define MOD3 vec3(443.8975, 397.2973, 491.1871) // uv range
         float hash11(float p) {
             vec3 p3  = fract(vec3(p) * MOD3);
             p3 += dot(p3, p3.yzx + 19.191);
             return fract((p3.x + p3.y) * p3.z);
         }
+        float sHash11(float a) {
+            return
+                mix(
+                    hash11(floor(a)),
+                    hash11(floor(a)+1.),
+                    smoothstep(0., 1., fract(a))
+                );
+        }
+
+        //	Classic Perlin 2D Noise 
+        //	by Stefan Gustavson
+        //
+        //  repetition of x added - stb
+        vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+        vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+
+        float cnoise(vec2 P, float rep){
+            P.x = mod(P.x, rep); // x rep 1/2
         
-        float sHash11(float p) {
-            return mix(hash11(p), hash11(p + 1.0), smoothstep(0.0, 1.0, p));
-            }
+        vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+            
+            Pi.z = mod(Pi.z, rep); // x rep 2/2
         
-        //classic Perlin noise by Stefan Gustavson
-        vec4 mod289(vec4 x) {
-            return x - floor(x * (1.0 / 289.0)) * 289.0;
+        vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+        Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+        vec4 ix = Pi.xzxz;
+        vec4 iy = Pi.yyww;
+        vec4 fx = Pf.xzxz;
+        vec4 fy = Pf.yyww;
+        vec4 i = permute(permute(ix) + iy);
+        vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+        vec4 gy = abs(gx) - 0.5;
+        vec4 tx = floor(gx + 0.5);
+        gx = gx - tx;
+        vec2 g00 = vec2(gx.x,gy.x);
+        vec2 g10 = vec2(gx.y,gy.y);
+        vec2 g01 = vec2(gx.z,gy.z);
+        vec2 g11 = vec2(gx.w,gy.w);
+        vec4 norm = 1.79284291400159 - 0.85373472095314 * 
+            vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+        g00 *= norm.x;
+        g01 *= norm.y;
+        g10 *= norm.z;
+        g11 *= norm.w;
+        float n00 = dot(g00, vec2(fx.x, fy.x));
+        float n10 = dot(g10, vec2(fx.y, fy.y));
+        float n01 = dot(g01, vec2(fx.z, fy.z));
+        float n11 = dot(g11, vec2(fx.w, fy.w));
+        vec2 fade_xy = fade(Pf.xy);
+        vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+        float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+        return 2.3 * n_xy;
         }
 
-        vec4 permute(vec4 x) {
-            return mod289(((x * 34.0) + 1.0) * x);
-        }
-
-        vec4 taylorInvSqrt(vec4 r) {
-            return 1.79284291400159 - 0.85373472095314 * r;
-        }
-
-        vec2 fadeEffect(vec2 t) {
-            return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
-        }
-        
-        float cnoise(vec2 P, float rep) {
-            P.x = mod(P.x, rep);
+        float Eye(vec2 p, float pupil, vec2 lpos) {
+            pupil += .1*(1.-length(lpos))-.1;
             
-            vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
             
-            Pi.z = mod(Pi.z, rep);
-            vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
-            Pi = mod289(Pi); // To avoid truncation effects in permutation
-            vec4 ix = Pi.xzxz;
-            vec4 iy = Pi.yyww;
-            vec4 fx = Pf.xzxz;
-            vec4 fy = Pf.yyww;
-
-            vec4 i = permute(permute(ix) + iy);
-
-            vec4 gx = fract(i * (1.0 / 41.0)) * 2.0 - 1.0 ;
-            vec4 gy = abs(gx) - 0.5 ;
-            vec4 tx = floor(gx + 0.5);
-            gx = gx - tx;
-
-            vec2 g00 = vec2(gx.x,gy.x);
-            vec2 g10 = vec2(gx.y,gy.y);
-            vec2 g01 = vec2(gx.z,gy.z);
-            vec2 g11 = vec2(gx.w,gy.w);
-
-            vec4 norm = taylorInvSqrt(vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11)));
-            g00 *= norm.x;
-            g01 *= norm.y;
-            g10 *= norm.z;
-            g11 *= norm.w;
-
-            float n00 = dot(g00, vec2(fx.x, fy.x));
-            float n10 = dot(g10, vec2(fx.y, fy.y));
-            float n01 = dot(g01, vec2(fx.z, fy.z));
-            float n11 = dot(g11, vec2(fx.w, fy.w));
-
-            vec2 fade_xy = fadeEffect(Pf.xy);
-            vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
-            float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
-            return 2.3 * n_xy;
-        }
-    
-        float Eye(vec2 p, float pupil, vec2 lpos){
-            pupil += 0.1 *(1.0 -length(lpos))-0.1;
+            //lpos.x = sin(time);
             
-            vec2 pr = vec2(atan(p.x,p.y)/PI/2.,clamp((length(p)-1.)/pupil+.8,0.,1.));
+            // radial coords
+            vec2 pr = vec2(atan(p.x, p.y) / PI / 2., clamp((length(p)-1.)/pupil+.8, 0., 1.));
             
-            //smooth curve from pupil to outer iris
-            pr.y = smoothstep(0.,1.,pr.y);
+            // smooth curve from pupil to outer iris
+            pr.y = smoothstep(0., 1., pr.y);
             
-            //noise frequency for ratial coords
-            vec2 freq = vec2(30.,1.5);
+            // noise frequency for radial coords
+            vec2 freq = vec2(30., 1.5);
             
-            //radial noise
-            float f = pow((cnoise(pr*freq,freq.x)+1.0)/4.0, 0.5);
-            f -= pow((cnoise(freq*pr*vec2(2.0,3.0)+9.0,2.0*freq.x)+1.0)/2.0-0.5, 2.0);
+            // radial noise
+            float f = pow((cnoise(pr*freq, freq.x)+1.)/4., .5);
             
-            //shading
-            float shade = dot(p,lpos);
+            // more radial noise
+            f -= 1.*pow((cnoise(pr*freq*vec2(2., 3.)+9., 2.*freq.x)+1.)/2.-.5, 2.);
             
-            //lightening shade of iris
+            //vec2 lpos = vec2(.5, .75);
+            
+            // general shading
+            float shade = dot(p, lpos);
+            
+            // lightening of iris
             f -= .7 * shade;
             
-            //darker inner iris & pupil
-            f *= pow(smoothstep(0.,.5,pr.y),.15);
+            // darker inner iris & pupil
+            f *= pow(smoothstep(0., .5, pr.y), .15);
             
             // darker ring around iris
-            f = mix(f, .25,smoothstep(.5,1.,pr.y+2.));
+            f = mix(f, .25, smoothstep(0.5, 1., pr.y+.2));
             
-            //mix in scleara
-            f = mix(f, 1.0 - 0.2 * dot(p, p) + 0.75 * shade, smoothstep(0.7, 0.85, pr.y));
+            // mix in sclera
+            f = mix(f, 1.-.2*dot(p, p)+.75*shade, smoothstep(0.7, .85, pr.y));
             
-            //highlight
-            //f = mix(1.0, f, clamp((length(p - lpos) - 0.15)/0.025, 0.0, 1.0));
+            // highlight
+            f = mix(1., f, clamp((length(p-lpos/1.)-.15)/.025, 0., 1.));
             
-            //eyelids
-            f = mix(f,0.,clamp((length(vec2(p.x,abs(p.y))+vec2(0.,1.3))-2.15)/.04,0.,1.));
+            // eyelids
+            f = mix(f, 0., clamp((length(vec2(p.x, abs(p.y))+vec2(0., 1.3))-2.15)/.04, 0., 1.));
             
             return f;
         }
-        
-        void main(void){
+
+        void main( void ) {
             ivec2 texel_coord = ivec2(gl_GlobalInvocationID.xy);
+
+            vec2 p = 3.* (vec2(texel_coord) - resolution / 2.0) / resolution.y;
             
-            // Correct the division operation and ensure all variables are properly declared and used
-            vec2 p = 5.* (texel_coord - resolution.xy / 2.0) / resolution.y;
-
-            // Correct the declaration of t
-            float t = 1.0 + 0.05 * sHash11(time * 1.5);
-
-            // Ensure position is declared and passed correctly
-            vec4 output_color = vec4(vec3(Eye(p, t, position*t)), 1.0);
-
-            imageStore(img_output, texel_coord, output_color);
+            // pupil contraction/expansion
+            float t = 1.+.05*sHash11(1.5*time);
             
+
+            vec4 value = vec4( vec3(Eye(p, t, position-.5)), 1.0 );
+        
+        imageStore(img_output, texel_coord, value);
+
         }
 
 
         """
-                
+    
+    elif emotion == 'Anger':
+        _compute_source = """#version 430 core
+
+        layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+
+        layout(rgba32f) uniform image2D img_output; // Output image
+
+        uniform vec2 resolution; // Texture resolution
+
+        void main() {
+            ivec2 texel_coord = ivec2(gl_GlobalInvocationID.xy); // Get the coordinate of the current texel
+            if (texel_coord.x >= resolution.x || texel_coord.y >= resolution.y) return; // Check boundary
+
+            // Define two colors for interpolation
+            vec3 colorA = vec3(1.0, 0.0, 0.0); // Red
+            vec3 colorB = vec3(0.0, 0.0, 1.0); // Blue
+
+            // Calculate the interpolation factor
+            float factor = smoothstep(0.0, resolution.x, float(texel_coord.x));
+
+            // Interpolate between colorA and colorB
+            vec3 color = mix(colorA, colorB, factor);
+
+            // Store the computed color in the image
+            imageStore(img_output, texel_coord, vec4(color, 1.0));
+        }
+
+"""
     else:
         _compute_source = """#version 430 core
         layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
@@ -603,9 +632,12 @@ def set_uniforms_for_shader(emotion,shader_program):
         
     elif emotion == 'Fear':
         shader_program['time'] = float(time.time() - start_time) 
-        # shader_program['resolution'] = (float(window.width),float(window.height))
-        shader_program['resolution'] = (2500.0,2500.0)
-        shader_program['position'] = (float(window.width/4),float(window.height/4))
+        shader_program['resolution'] = (float(window.width),float(window.height))
+        # shader_program['resolution'] = (2500.0,2500.0)
+        shader_program['position'] = (float(window._mouse_x),float(window._mouse_y))
+        
+    elif emotion == 'Anger':
+        shader_program['resolution'] = (float(window.width),float(window.height))
         
     else:
         shader_program['time'] = float(time.time() - start_time)
